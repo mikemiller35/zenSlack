d96e101e07a71812fc30e8e1d403bcf58a5d16f0
Mike Miller <mmiller@levelsbeyond.com>
2020-09-30 13:02:59 -0600

    getting there with what we want

--- slack-notify/handler.py
+++ slack-notify/handler.py
@@ -1,17 +1,23 @@
 import os
 import json
-from botocore.vendored import requests
+import unzip_requirements
+import requests
 
 
 def lambda_handler(event, context):
-    payload = {"text": "Someone hit the endpoint!"}
+
+    print(event)
+
+    submit_user = json.loads(event.get("body")).get("ticket_quester")
+
+    payload = {"text": "Someone named " + submit_user + " put in a ticket!"}
     slackhook = os.environ.get("SLACKHOOK")
     requests.post(slackhook, json.dumps(payload))
 
     response = {
         "statusCode": 200,
         "headers": {
-            "Foo": "bar",
+            "Posted-To-Slack": "True",
         },
     }
 
