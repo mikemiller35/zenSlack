7d42cb1cc5ee03e47185ff1754e20c1e04686fc9
Mike Miller <mmiller@levelsbeyond.com>
2020-09-30 11:40:17 -0600

    changes

--- slack-notify/handler.py
+++ slack-notify/handler.py
@@ -1,12 +1,18 @@
 import os
 import json
+from botocore.vendored import requests
+
 
 def lambda_handler(event, context):
+    payload = {"text": "Someone hit the endpoint!"}
+    slackhook = os.environ.get("SLACKHOOK")
+    requests.post(slackhook, json.dumps(payload))
+
     response = {
         "statusCode": 200,
         "headers": {
             "Foo": "bar",
         },
     }
-        
+
     return response
